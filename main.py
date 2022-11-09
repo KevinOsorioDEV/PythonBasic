c5987d30d86dba8fb90c5c45aac7f29d640b518f
KevinDevop <110274931+KevinDevop@users.noreply.github.com>
2022-11-09 10:48:14 -0500

    Commit 2
    Se está puliendo el código, pero ya hace su propósito.

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import time
 
 
 def bumeran():
-    arra1 = [2, 1, 2, 3, 3, 4, 2, 4, 2]
+    arra1 = [2, 1, 2, 3, "a", 3, 4, 2, 4, 2]
 
     try:
         for i in range(len(arra1)):
             if arra1[i] == arra1[i + 2]:
                 print(arra1[i], arra1[i + 1], arra1[i + 2])
-            else:
-                print("")
+
     except IndexError:
-        print("EL proceso terminó")
+        print("El proceso terminó")
 
 
 if __name__ == '__main__':
