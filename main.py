ce99cb48776425c797f9c1075d5d9e1cb2616fcb
KevinDevop <110274931+KevinDevop@users.noreply.github.com>
2022-11-09 11:27:43 -0500

    Mejora del codigo.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import time
-
 
 def bumeran():
     arra1 = [2, 1, 2, 3, "a", 3, 4, 2, 4, 2]
 
     try:
         for i in range(len(arra1)):
-            if arra1[i] == arra1[i + 2]:
+            if arra1[i] == arra1[i + 2] and arra1[i] != arra1[i + 1]:
                 print(arra1[i], arra1[i + 1], arra1[i + 2])
 
     except IndexError:
